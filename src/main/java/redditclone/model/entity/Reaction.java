0790557d45b91eb97c7673fe72f2fbd7744f7a23
Dorian Janjic <80455155+doka900@users.noreply.github.com>
2022-08-13 17:39:28 +0200

    Update Reaction

--- src/main/java/redditclone/model/entity/Reaction.java
+++ src/main/java/redditclone/model/entity/Reaction.java
@@ -2,18 +2,45 @@ package redditclone.model.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="reaction")
 public class Reaction {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long reaction_id;
 	
-	private ReactionType type;
-
+	@Enumerated(EnumType.STRING)
+	@Column(name = "reaction_type")
+	private ReactionType reaction_type;
+	
+	@Column
 	private LocalDate time_stamp;
-
+	
+	@Column
 	private long voter_id;
-
+	
+	@Column
 	private long post_id;
 	
 	
